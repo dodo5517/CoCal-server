2f2c11b7d78346e16e02a15e38821f8f03851668
dodo5517 <kh9690754@naver.com>
2025-09-29 15:01:45 +0900

    build: 프로젝트 생성

---
+++ src/main/groovy/cola/springboot/cocal/CocalApplication.groovy
@@ -0,0 +1,13 @@
+package cola.springboot.cocal
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CocalApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CocalApplication, args)
+    }
+
+}
---
+++ src/test/groovy/cola/springboot/cocal/CocalApplicationTests.groovy
@@ -0,0 +1,13 @@
+package cola.springboot.cocal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
