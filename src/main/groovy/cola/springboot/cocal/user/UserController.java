b72afaf46bd06adb097f1b350f688eedc714e0a0
sungAh123 <bobaah0832@gmail.com>
2025-10-01 19:43:33 +0900

    feat: UserController 일반 회원가입 기능 추가

--- src/main/groovy/cola/springboot/cocal/user/UserController.java
+++ src/main/groovy/cola/springboot/cocal/user/UserController.java
@@ -1,4 +1,22 @@
 package cola.springboot.cocal.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping()
+    public User signUp(@RequestBody User user) {
+        return userService.signUp(user);
+    }
 }
