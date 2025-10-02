a5c2201cb59ed9df143f18582480e84c0a05a034
sungAh123 <bobaah0832@gmail.com>
2025-10-02 13:41:02 +0900

    feat: 구글 로그인 기능 구현

---
+++ src/main/groovy/cola/springboot/cocal/user/CustomOAuth2UserService.java
@@ -0,0 +1,42 @@
+package cola.springboot.cocal.user;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+
+        User user = userRepository.findByEmail(email)
+                .orElseGet(() -> {
+                    User newUser = new User();
+                    newUser.setEmail(email);
+                    newUser.setName(name);
+                    newUser.setProvider(User.Provider.GOOGLE);
+                    newUser.setCreatedAt(LocalDateTime.now());
+                    newUser.setUpdatedAt(LocalDateTime.now());
+                    // 프로필 이미지 URL 가져오기
+                    String picture = oAuth2User.getAttribute("picture");
+                    newUser.setProfileImageUrl(picture);
+                    return userRepository.save(newUser);
+                });
+
+        return oAuth2User;
+    }
+}
