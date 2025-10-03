530b2d2f3e39e857c36b1d2423cdde393f65cad4
sungAh123 <bobaah0832@gmail.com>
2025-10-04 00:29:15 +0900

    feat: /api/project POST 엔드포인트 구현, 프로젝트 생성 API 컨트롤러 구현

--- src/main/groovy/cola/springboot/cocal/project/ProjectController.java
+++ src/main/groovy/cola/springboot/cocal/project/ProjectController.java
@@ -1,5 +1,7 @@
 package cola.springboot.cocal.project;
 
+import cola.springboot.cocal.user.User;
+import cola.springboot.cocal.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,18 +9,28 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/project")
 @RequiredArgsConstructor
 public class ProjectController {
+
     private final ProjectService projectService;
+    private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
+
+    // 프로젝트 생성
+    @PostMapping()
+    public ResponseEntity<Project> createProject(@RequestBody Project project, Authentication authentication) {
+        Long userId = Long.parseLong(authentication.getName()); // User ID 가져오기
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        project.setOwner(user); // owner에 로그인 사용자 세팅
+        Project saved = projectRepository.save(project);
+        return ResponseEntity.ok(saved);
+    }
 
     // project 목록 조회
     @GetMapping()
@@ -34,4 +46,7 @@ public class ProjectController {
         Page<ProjectResponseDto> projects = projectService.getProjects(pageable);
         return ResponseEntity.ok(projects);
     }
+
 }
+
+
