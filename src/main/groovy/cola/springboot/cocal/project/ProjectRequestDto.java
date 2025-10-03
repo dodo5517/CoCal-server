b965aa2d5d5b5fd87fb8904ccc552c5ed7d6d450
sungAh123 <bobaah0832@gmail.com>
2025-10-04 00:28:31 +0900

    feat: 프로젝트 생성/수정 요청 DTO 구현

--- src/main/groovy/cola/springboot/cocal/project/ProjectRequestDto.java
+++ src/main/groovy/cola/springboot/cocal/project/ProjectRequestDto.java
@@ -1,4 +1,21 @@
 package cola.springboot.cocal.project;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ProjectRequestDto {
+    private String name;
+    private LocalDate startDate;
+
+    @FutureOrPresent(message = "endDate는 오늘 이후여야 합니다.")
+    private LocalDate endDate;
+
+    private String status;  // 생략 시 서버에서 기본값 적용
 }
