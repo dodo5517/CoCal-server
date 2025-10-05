572e577c6e8f756d863b9291d012545e4c63c4fe
dodo5517 <kh9015548@gmail.com>
2025-10-06 00:45:14 +0900

    feat: 초대 링크 수락 api 구현

--- src/main/groovy/cola/springboot/cocal/invite/InviteController.java
+++ src/main/groovy/cola/springboot/cocal/invite/InviteController.java
@@ -5,10 +5,10 @@ import cola.springboot.cocal.invite.dto.InviteResolveResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +24,17 @@ public class InviteController {
             HttpServletRequest req
     ) {
         InviteResolveResponse data = inviteService.resolve(token);
+        return ResponseEntity.ok(ApiResponse.ok(data, req.getRequestURI()));
+    }
+
+    // 초대 수락
+    @PostMapping("/accept")
+    public ResponseEntity<ApiResponse<Map<String, String>>> accept(Authentication auth, HttpServletRequest req,
+                                                                   @RequestBody Map<String, String> body){
+        Long userId = Long.parseLong(auth.getName());
+        String token = body.get("token");
+        inviteService.acceptLink(token, userId);
+        Map<String, String> data = Map.of("message", "초대를 수락했습니다.");
         return ResponseEntity.ok(ApiResponse.ok(data, req.getRequestURI()));
     }
 }
