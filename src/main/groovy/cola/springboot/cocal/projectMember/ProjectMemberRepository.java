0f7f3d66ddcc047278a3d1ef5a76fbd045a5e068
dodo5517 <kh9015548@gmail.com>
2025-10-05 14:07:37 +0900

    feat: ProjectMemberRepository 생성

---
+++ src/main/groovy/cola/springboot/cocal/projectMember/ProjectMemberRepository.java
@@ -0,0 +1,6 @@
+package cola.springboot.cocal.projectMember;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
+}
