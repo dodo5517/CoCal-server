a8ced2576bac3c2569a7aacef1396fd8a99d0f2a
sungAh123 <bobaah0832@gmail.com>
2025-10-05 17:45:10 +0900

    feat: merge 하면서 생긴 중복 내용 제거

--- src/main/groovy/cola/springboot/cocal/invite/InviteRepository.java
+++ src/main/groovy/cola/springboot/cocal/invite/InviteRepository.java
@@ -52,6 +52,7 @@ public interface InviteRepository extends JpaRepository<Invite, Long> {
             @Param("projectId") Long projectId,
             @Param("statuses") Collection<Invite.InviteStatus> statuses
     );
+
     // 사용자가 해당 프로젝트 초대 수락했는지 확인
     @Query("""
         SELECT COUNT(i) > 0 
@@ -61,20 +62,5 @@ public interface InviteRepository extends JpaRepository<Invite, Long> {
             AND i.status = 'ACCEPTED' 
     """)
     boolean existsAcceptedInvite(@Param("projectId") Long projectId, @Param("email") String email);
-
-    // 프로젝트의 초대 상태가 PENDING인 초대 요청만 조회
-    @Query("""
-        select i
-        from Invite i
-        join fetch i.project p
-        left join fetch i.invitedBy inviter
-        where p.id = :projectId
-          and i.status in :statuses
-        order by i.createdAt desc
-    """)
-    List<Invite> findByProjectAndStatuses(
-            @Param("projectId") Long projectId,
-            @Param("statuses") Collection<Invite.InviteStatus> statuses
-    );
 
 }
