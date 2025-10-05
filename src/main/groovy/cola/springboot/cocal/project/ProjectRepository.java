72bd7f61afdbc6df9b05d66af2a8d115ca7d32a9
sungAh123 <bobaah0832@gmail.com>
2025-10-05 11:31:49 +0900

    feat: 소유자 또는 초대 수락한 사용자 기준으로 프로젝트 조회하는 쿼리 메서드 추가

--- src/main/groovy/cola/springboot/cocal/project/ProjectRepository.java
+++ src/main/groovy/cola/springboot/cocal/project/ProjectRepository.java
@@ -1,6 +1,25 @@
 package cola.springboot.cocal.project;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    // 내가 생성한 프로젝트 or 내가 초대 수락한 프로젝트 조회
+    @Query(
+            "SELECT DISTINCT p " +
+                    "FROM Project p " +
+                    "LEFT JOIN Invite i ON i.project = p " +
+                    "WHERE p.owner.id = :userId " +
+                    "OR (i.email = :email AND i.status = 'ACCEPTED')"
+    )
+    Page<Project> findMyProjects(
+            @Param("userId") Long userId,
+            @Param("email") String email,
+            Pageable pageable
+    );
+
 }
