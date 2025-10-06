2fb338cdb9e215eb45189dc1aa5b69b8a44b9c09
sungAh123 <bobaah0832@gmail.com>
2025-10-06 17:48:14 +0900

    feat: 프로젝트 ID와 이벤트 ID로 이벤트 조회 쿼리 메서드 추가

--- src/main/groovy/cola/springboot/cocal/event/EventRepository.java
+++ src/main/groovy/cola/springboot/cocal/event/EventRepository.java
@@ -1,6 +1,11 @@
 package cola.springboot.cocal.event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    Optional<Event> findByIdAndProjectId(Long id, Long projectId);
 }
