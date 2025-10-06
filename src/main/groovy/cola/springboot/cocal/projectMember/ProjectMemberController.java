449802dfcac6a2ddd31dc6cf84d13e7d13993348
dodo5517 <kh9015548@gmail.com>
2025-10-06 19:58:37 +0900

    feat: 팀원 추방 api 구현

---
+++ src/main/groovy/cola/springboot/cocal/projectMember/ProjectMemberController.java
@@ -0,0 +1,31 @@
+package cola.springboot.cocal.projectMember;
+
+import cola.springboot.cocal.common.api.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/project/{projectId}/team")
+public class ProjectMemberController {
+    private final ProjectMemberService memberService;
+
+    // 프로젝트 팀원 강제추방
+    @PostMapping("/{userId}/kick")
+    public ResponseEntity<ApiResponse<Map<String, String>>> kick(
+            @PathVariable Long projectId,
+            @PathVariable Long userId,
+            Authentication auth,
+            HttpServletRequest httpReq
+    ) {
+        Long actorUserId = Long.parseLong(auth.getName());
+        memberService.kick(actorUserId, projectId, userId);
+        Map<String, String> data = Map.of("message", "팀원을 추방했습니다.");
+        return ResponseEntity.ok(ApiResponse.ok(data, httpReq.getRequestURI()));
+    }
+}
