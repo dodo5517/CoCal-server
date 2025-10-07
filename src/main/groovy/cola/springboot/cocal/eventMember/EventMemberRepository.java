d571f89ea0051ec00f68482bdce9558bce0b75bf
sungAh123 <bobaah0832@gmail.com>
2025-10-07 15:56:42 +0900

    feat: 참가자 조회용 메서드 추가

--- src/main/groovy/cola/springboot/cocal/eventMember/EventMemberRepository.java
+++ src/main/groovy/cola/springboot/cocal/eventMember/EventMemberRepository.java
@@ -1,9 +1,22 @@
 package cola.springboot.cocal.eventMember;
 
+import cola.springboot.cocal.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EventMemberRepository extends JpaRepository<EventMember, EventMemberId> {
     boolean existsById_EventIdAndId_UserId(Long eventId, Long userId);
 
     void deleteById_EventIdAndId_UserId(Long eventId, Long userId);
+
+    @Query("""
+        select  em.user
+        from EventMember  em
+        join em.event e
+        where e.id = :eventId
+    """)
+    List<User> findUsersByEventId(@Param("eventId") Long eventId);
 }
