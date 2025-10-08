95dc0afcb37d3623f3f7481d2ca2eb0d97046c14
dodo5517 <kh9015548@gmail.com>
2025-10-08 13:17:35 +0900

    feat: 해당 프로젝트+날짜의 메모 찾는 쿼리 구현

--- src/main/groovy/cola/springboot/cocal/memo/MemoRepository.java
+++ src/main/groovy/cola/springboot/cocal/memo/MemoRepository.java
@@ -1,6 +1,27 @@
 package cola.springboot.cocal.memo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+   
+    // 해당 프로젝트+날짜의 메모 찾기
+    @Query("""
+        select m
+        from Memo m
+        join m.project p
+        where p.id = :projectId
+          and m.memoDate = :memoDate
+        order by m.createdAt desc, m.id desc
+    """)
+    Page<Memo> findByProjectAndDate(
+            @Param("projectId") Long projectId,
+            @Param("memoDate") LocalDate memoDate,
+            Pageable pageable
+    );
 }
