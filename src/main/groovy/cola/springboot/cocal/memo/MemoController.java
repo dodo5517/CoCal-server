d6f7dc20dc999f036af0bfea6c02d6141a6d9e29
dodo5517 <kh9015548@gmail.com>
2025-10-08 13:18:56 +0900

    feat: 해당 프로젝트+날짜의 메모 조회 api 구현

--- src/main/groovy/cola/springboot/cocal/memo/MemoController.java
+++ src/main/groovy/cola/springboot/cocal/memo/MemoController.java
@@ -6,9 +6,16 @@ import cola.springboot.cocal.memo.DTO.MemoResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +32,20 @@ public class MemoController {
                                                                 HttpServletRequest httpReq) {
         Long userId = Long.parseLong(auth.getName());
         MemoResponse data = memoService.createMemo(projectId, userId, req);
+        return ResponseEntity.ok(ApiResponse.ok(data, httpReq.getRequestURI()));
+    }
+
+    // 메모 조회
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<MemoResponse>>> listByDate(
+            @PathVariable Long projectId,
+            @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+            @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            Authentication auth,
+            HttpServletRequest httpReq
+    ) {
+        Long userId = Long.parseLong(auth.getName());
+        Page<MemoResponse> data = memoService.getMemosByDate(projectId, userId, date, pageable);
         return ResponseEntity.ok(ApiResponse.ok(data, httpReq.getRequestURI()));
     }
 }
