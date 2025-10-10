1c564a678c3089f69bc561c4077f5a4af3cb8056
sungAh123 <bobaah0832@gmail.com>
2025-10-10 23:15:19 +0900

    feat: findAllByOwnerId 메서드 구현

--- src/main/groovy/cola/springboot/cocal/todo/private_todo/PrivateTodoRepository.java
+++ src/main/groovy/cola/springboot/cocal/todo/private_todo/PrivateTodoRepository.java
@@ -2,5 +2,8 @@ package cola.springboot.cocal.todo.private_todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PrivateTodoRepository extends JpaRepository<PrivateTodo, Long>{
+    List<PrivateTodo> findAllByOwnerId(Long ownerId);
 }
