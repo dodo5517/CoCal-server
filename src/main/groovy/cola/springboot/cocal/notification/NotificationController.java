017d56c2bd767907b26f453031598263efdb2306
sungAh123 <bobaah0832@gmail.com>
2025-10-14 15:58:29 +0900

    feat: 알림 조회, 읽기 api 구현

---
+++ src/main/groovy/cola/springboot/cocal/notification/NotificationController.java
@@ -0,0 +1,48 @@
+package cola.springboot.cocal.notification;
+
+import cola.springboot.cocal.common.api.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notifications")
+@RequiredArgsConstructor
+public class NotificationController {
+    private final NotificationService notificationService;
+
+    // 읽지 않은 알림 조회
+    @GetMapping("/unread")
+    public ResponseEntity<ApiResponse<List<Notification>>> getUnreadNotifications(Authentication auth,
+                                                                                  HttpServletRequest req) {
+        Long userId = Long.parseLong(auth.getName()); // JWT 등에서 로그인 사용자 ID 가져오기
+        List<Notification> notifications = notificationService.getUnreadNotifications(userId);
+        return ResponseEntity.ok(ApiResponse.ok(notifications, req.getRequestURI()));
+    }
+
+    // 특정 사용자의 읽지 않은 알림 조회
+    @GetMapping("/unread/{userId}")
+    public ResponseEntity<ApiResponse<List<Notification>>> getUnread(
+            @PathVariable("userId") Long userId,
+            HttpServletRequest req)
+    {
+        List<Notification> notifications = notificationService.getUnreadNotifications(userId);
+        return ResponseEntity.ok(ApiResponse.ok(notifications, req.getRequestURI()));
+    }
+
+
+    // 알림 읽음 처리
+    @PostMapping("/read/{notificationId}")
+    public ResponseEntity<ApiResponse<Void>> markAsRead(
+            @PathVariable Long notificationId,
+            HttpServletRequest req
+    ) {
+        notificationService.markAsRead(notificationId);
+        return ResponseEntity.ok(ApiResponse.ok(null, req.getRequestURI()));
+    }
+
+}
