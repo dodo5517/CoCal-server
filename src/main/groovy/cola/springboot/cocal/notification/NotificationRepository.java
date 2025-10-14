a1c3c9e53c91804bb00d293ea7608c94c4771af1
sungAh123 <bobaah0832@gmail.com>
2025-10-14 15:56:31 +0900

    feat: NotificationRepository 기본 쿼리 메서드 구현

--- src/main/groovy/cola/springboot/cocal/notification/NotificationRepository.java
+++ src/main/groovy/cola/springboot/cocal/notification/NotificationRepository.java
@@ -1,4 +1,16 @@
 package cola.springboot.cocal.notification;
 
-public class NotificationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findAllByUserIdAndIsReadFalse(Long userId);
+
+    // 특정 유저, 특정 이벤트, 특정 타입 알림이 이미 존재하는지 체크
+    boolean existsByUserIdAndReferenceIdAndType(Long userId, Long referenceId, String type);
+
+    void deleteAllByReferenceIdAndTypeAndIsReadFalse(Long referenceId, String type);
 }
