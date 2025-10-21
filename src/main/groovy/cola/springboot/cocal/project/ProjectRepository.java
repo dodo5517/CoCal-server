14ee5cd1a090034064325a785692158ff63b9687
dodo5517 <kh9015548@gmail.com>
2025-10-22 00:40:35 +0900

    fix: project_members의 status가 "ACTIVE"인 프로젝트 목록만 조회

--- src/main/groovy/cola/springboot/cocal/project/ProjectRepository.java
+++ src/main/groovy/cola/springboot/cocal/project/ProjectRepository.java
@@ -8,14 +8,15 @@ import org.springframework.data.repository.query.Param;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
-    // 내가 생성한 프로젝트 or 내가 초대 수락한 프로젝트 조회
-    @Query(
-            "SELECT DISTINCT p " +
-                    "FROM Project p " +
-                    "LEFT JOIN Invite i ON i.project = p " +
-                    "WHERE p.owner.id = :userId " +
-                    "OR (i.email = :email AND i.status = 'ACCEPTED')"
-    )
+    // 내가 생성한 프로젝트 or 내가 초대 수락한 프로젝트 조회. 단, 내가 active인 프로잭트만
+    @Query("""
+        SELECT DISTINCT p
+        FROM Project p
+        LEFT JOIN Invite i ON i.project = p
+        LEFT JOIN ProjectMember pm ON pm.project = p
+        WHERE p.owner.id = :userId
+           OR (pm.user.id = :userId AND pm.status = 'ACTIVE')
+    """)
     Page<Project> findMyProjects(
             @Param("userId") Long userId,
             @Param("email") String email,
